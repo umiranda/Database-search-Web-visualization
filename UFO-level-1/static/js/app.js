b79ca7ede25f0f4917908cfa78ae1bb9efef6a14
umiranda <71298635+umiranda@users.noreply.github.com>
2021-01-11 23:12:45 -0500

    tt

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,6 +2,42 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+//filtering to gegt the data
+var button = d3.select("#button");
 
+var form = d3.select("#form");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter () {
+    d3.event.preventDefault();
+
+    var inpuElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    var filteredData = tableData.filter(date => date.datetime === inputValue);
+
+    console.log(filteredData);
+
+}
+
+
+
+//----------------------
+//looping through to create the table
 var tbody = d3.select("tbody");
-data
+console.log(data);
+
+data.forEach( function(ufoReport){
+        var row = tbody.append("tr")
+        Object.entries(ufoReport).forEach( function ([key,value]){
+            row.append("td").text(value)
+}
+)
+})
+
