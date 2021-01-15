63a3222219bbc885ff143837c418f73e9101b64f
umiranda <71298635+umiranda@users.noreply.github.com>
2021-01-14 23:19:16 -0500

    final commit

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -16,32 +16,35 @@ var tableData = data;
 
 var button = d3.select("#filter-btn");
 
-var form = d3.select("#form");
+var form = d3.select("#datetime");
+
 
 button.on("click", runEnter);
 form.on("submit", runEnter);
 
-d3.select("tbody").html("");
+//d3.select("tbody").html("");
 
 function runEnter () {
     
-    d3.event.preventDefault();
+    //d3.event.preventDefault();
 
     var dateTime = d3.select("#datetime").property("value");
     console.log(dateTime);
 
     var filteredData = tableData.filter(record => record.datetime === dateTime);
-    //console.log(filteredData);
-
+    console.log(filteredData);
+    d3.select("tbody").html("");
+    
+    
     filteredData.forEach((ufoReport) => {
         var row = tbody.append('tr');
     
         Object.entries(ufoReport).forEach(([key, value]) => {
-            //console.log(key, value);
+            console.log(key, value);
             var cell = row.append('td');
             cell.text(value);
         });
     });
     
-
+    
 };
